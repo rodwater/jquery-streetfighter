cec778ff1dc8e1a5cf314ba3a5a3ea7a695303ef
Rodwater <rodwater@gmail.com>
2016-04-28 13:01:50 -0700

    js

---
+++ js/app.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+
+
+
+$('.ryu').mouseenter(function(){
+	$('.ryu-still').hide();
+	$('.ryu-ready').show();
+	})
+	.mouseleave(function(){
+	$('.ryu-still').show();
+	$('.ryu-ready').hide();
+	})
+	.mousedown(function(){
+	//console.log('mousedown');
+	// play hadouken sound
+	playHadouken();
+	$('.ryu-ready').hide();
+	$('.ryu-throwing').show();
+	$('.hadouken').finish().show().animate({
+		'left': '1020px'}, 
+		500,
+		function(){
+			$(this).hide();
+			$(this).css('left', '520px');
+		});
+	// show hadouken and animate it to the right of the screen	
+	})
+	.mouseup(function(){
+	//console.log('mouseup');
+	$('.ryu-throwing').hide();
+	$('.ryu-ready').show();
+	// ryu goes back to ready position
+	});
+
+});
+
+function playHadouken () {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+}
